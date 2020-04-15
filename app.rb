75d4f9c28da14acdf1ad55c73866eec88f089eb3
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-15 18:27:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].split.reverse
+    "#{@name}"
+  end
+
 
 end
