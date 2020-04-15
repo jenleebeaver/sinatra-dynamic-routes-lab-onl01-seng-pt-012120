cd5f2b0aad6596deca158a8a1e130e52d5e534e6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-15 18:37:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,10 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     answer = ''
-    params[:phrase].to_i.times
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+    answer
   end
 
 
